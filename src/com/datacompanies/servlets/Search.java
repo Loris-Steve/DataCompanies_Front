4e60f010e75b48010a4aa0b1a230ba90d07be253
David-Charlesia <pro.charlesia.david@outlook.com>
2021-02-01 22:48:38 +0100

    modif Search java

--- src/com/datacompanies/servlets/Search.java
+++ src/com/datacompanies/servlets/Search.java
@@ -40,9 +40,9 @@ public class Search extends HttpServlet {
 	private Map<String, String> mapCodeToName ;
 	private Map<String, String> mapNmaeToCode ;
 
-	private MongoCursor<Document> result;
+	private MongoCursor<Document> cursor;
     private JSONObject queryUser = new JSONObject();
-    private ArrayList<HashMap> listeAnnualAccounts = new ArrayList<HashMap>();  
+    private ArrayList<AnnualAccount> accountList;
 	
     private static final long serialVersionUID = 1L;
 	
@@ -85,18 +85,12 @@ public class Search extends HttpServlet {
 			// on créer une requête avec les valeurs saisies par l'utilisateur
 			queryUser = searchAccountform.verifyAttributs(request);
 			Requests req = new Requests("ec2-3-138-34-23.us-east-2.compute.amazonaws.com");
-			result = req.doRequest(queryUser);
+			cursor = req.doRequest(queryUser);
 			
-			/*Document doc = new Document() ;
-			System.out.println("resultat");
-			doc = result.next();
 
-        	System.out.println("#i "+doc.toString() );
-	    	System.out.println("TEST AFFICHE !"+doc.get("DD").toString());*/
-			//sendListAcount(request);
-			getListenerAnnuaalAccount(result, request);
+			getListenerAnnuaalAccount(request);
 		} catch (BeanException e) {
-			request.setAttribute("error", "Formulaire incorrecte");//+e.getMessage());
+			request.setAttribute("error", "Formulaire incorrecte");
 			
 		}
 
@@ -105,78 +99,34 @@ public class Search extends HttpServlet {
 	}
 		
 	public void sendListAcount(HttpServletRequest request){
-
-		Map<String,String> attributsMap = new HashMap<String,String>();
-		Map<String,String> attributsMap2 = new HashMap<String,String>();
-
-		//attributsMap.put("siren", "bye");
-		attributsMap.put("fonds_commercial", "good");
-		attributsMap2.put("actif", "chantaaal");
-listeAnnualAccounts.add((HashMap) attributsMap);
-listeAnnualAccounts.add((HashMap) attributsMap2);
-/*doc.put("siren", "super");
-doc.put("fonds_commercial", "vraiment");
-doc.put("actif", "chantaaal");
-*/
 		request.setAttribute("listeAnnualAccounts", listeAnnualAccounts);
 		//*System.out.println( "debut du get"+annualAccountDao.listener());
 		//request.setAttribute("companiesww", annualAccountDao.listener());
 	}
 	
-	public void getListenerAnnuaalAccount(MongoCursor<Document> cursor,HttpServletRequest request){
-	/*	Document doc = new Document() ;
-		System.out.println("resultat");
-		doc = cursor.next();
+	public void getListenerAnnuaalAccount(HttpServletRequest request){
+	
+		System.out.println("In getListenerAnnuaalAccount method");
 
-    	System.out.println("#i "+doc.toString() );
-    	System.out.println("TEST AFFICHE !"+doc.get("DD").toString());*/
-		Document doc = new Document() ;
-		System.out.println("resultat");
+		this.accountList = new ArrayList();
+		this.fillAccountList();
+		
+		request.setAttribute("listeAnnualAccounts", accountList);
+
+	}
+
+	public void nextPage(int nbNextPages){
+		for(int i=0;i<nbNextPages;i++){
+			this.fillAccountList(this.accountList);
+		}
+	}
+
+	private void fillAccountList(){
 		for(int i=0;i<10;i++){
-			    //while(cursor.hasNext()){
-			if(cursor.hasNext()) {
-				doc = cursor.next();
-				System.out.println("#i "+doc.toString() );
-		    	System.out.println("TEST AFFICHE !");//+doc.get("DD").toString());
-
-
-	        	Map<String, String> annualAccount = new HashMap<String, String>() ;
-	        	// je parcours la liste des attribut que le compte de l'entreprise peut contenir
-	            //Parcourir le Hashmap avec la boucle For
-	            for (Map.Entry a : mapAttributs.entrySet()) {
-	                System.out.println("ID: "+a.getKey()+", Nom: "+a.getValue());
-					if(doc.get(a.getValue()) != null){
-		                System.out.println(a.getKey()+"Enter"+doc.get(a.getValue()));
-		                /*
-		                if(doc.get(a.getValue()) instanceof ArrayList) {
-		                	System.out.println("voi Arraylist");
-
-			               ArrayList<Double> list = (ArrayList<Double>) doc.get(a.getValue());
-		                	System.out.println("val"+list.get(0));
-		                	Double val = list.get(0);
-		                	annualAccount.put((String) a.getKey(),String.valueOf(val));
-
-		                }
-		                else if(doc.get(a.getValue()) instanceof Object) {
-		                	System.out.println("voi objet");
-			               // ArrayList val = (ArrayList) doc.get(a.getValue());
-			                System.out.println("val");
-		                	//annualAccount.put((String) a.getKey(), String.valueOf(val.get(0)));
-		                }
-		                else if(doc.get(a.getValue()) instanceof String){
-		                	System.out.println("Autre");
-
-		                	
-		                }
-		               */
-		                annualAccount.put((String) a.getKey(),doc.get(a.getValue()).toString());
-					}
-	            }
-		        listeAnnualAccounts.add((HashMap) annualAccount);
-		    }
-}
-		request.setAttribute("listeAnnualAccounts", listeAnnualAccounts);
-
+			if(this.cursor.hasNext()){
+				this.accountList.add(new AnnualAccount(this.cursor.next()));
+			}
+		}
 	}
 	
 	/*public String getCodeAttribut(String key) {
